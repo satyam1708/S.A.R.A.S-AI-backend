27a01e9d54213cae8f2cb8752485cdd4ae8262bc
satyam1708 <pandeysatyam1708@gmail.com>
2025-11-18 18:09:10 +0530

    fix

--- src/modules/exams/exams.routes.js
+++ src/modules/exams/exams.routes.js
@@ -1,15 +1,16 @@
 import express from 'express';
 import * as examController from './exams.controller.js';
-import { authenticateUser, authorizeAdmin } from '../../middleware/auth.middleware.js';
+import { authMiddleware } from '../../middleware/auth.middleware.js';
+import {adminMiddleware} from '../../middleware/admin.middleware.js';
 
 const router = express.Router();
 
 // Student Routes
-router.get('/', authenticateUser, examController.listMocks);
-router.post('/submit', authenticateUser, examController.submitExam);
+router.get('/', authMiddleware, examController.listMocks);
+router.post('/submit', authMiddleware, examController.submitExam);
 
 // Admin Routes (Generation & Upload)
-router.post('/generate/:courseId', authenticateUser, authorizeAdmin, examController.generateMock);
-router.post('/upload-pyq', authenticateUser, authorizeAdmin, examController.uploadPYQ);
+router.post('/generate/:courseId', authMiddleware, adminMiddleware, examController.generateMock);
+router.post('/upload-pyq', authMiddleware, adminMiddleware, examController.uploadPYQ);
 
 export default router;
